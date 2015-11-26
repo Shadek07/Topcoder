79c317a3225fae4982aba5caebf97a38251b01c4
Md. Shadekur Rahman <shadekcse07@gmail.com>
2015-11-27 00:05:54 +0600

    div1 300 on Combinatorics

---
+++ SRM 673/BearCavalry.cpp
@@ -0,0 +1,192 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+#define MOD 1000000007
+
+class BearCavalry {
+    public:
+    int countAssignments(vector<int> warriors, vector<int> horses) 
+    {
+        int n = horses.size();
+        int i,j,k;
+        long long int ans = 0;
+        
+        forl(i,0,n){
+            int streng = warriors[0]*horses[i];
+            vector<int> cnt(n-1,0);
+            vector<int> a;
+            forl(j,1,n){
+               a.pb(warriors[j]);
+            }
+            vector<int> h;
+            forl(j,0,n){
+                if(j != i){
+                    h.pb(horses[j]);
+                }
+            }
+            forl(j,0,n-1){
+                forl(k,0,n-1){
+                    if(a[j]*h[k] < streng){
+                        cnt[j]++;
+                    }
+                }
+            }
+            sort(all(cnt));
+            int tmp = 1;
+            forl(j,0,n-1){
+                int way = cnt[j]-j;
+                if(way <= 0){
+                    tmp = 0;
+                    break;
+                }
+                tmp =(long long) tmp*way%MOD;
+            }
+            ans = (ans+tmp)%MOD;
+        }
+        return ans;
+    }
+};
+
+
+// CUT begin
+ifstream data("BearCavalry.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> warriors, vector<int> horses, int __expected) {
+    time_t startClock = clock();
+    BearCavalry *instance = new BearCavalry();
+    int __result = instance->countAssignments(warriors, horses);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> warriors;
+        from_stream(warriors);
+        vector<int> horses;
+        from_stream(horses);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(warriors, horses, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1448559837;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 300 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "BearCavalry (300 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
