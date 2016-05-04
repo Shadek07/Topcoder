96e2ef71c6a2de1e191a981079d05cea09aafac3
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-05-04 16:56:42 +0600

    (div2 medium) prefix sum + simple math

---
+++ Single Round Match 690/GerrymanderEasy.cpp
@@ -0,0 +1,174 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+#define INF 9999999
+
+
+class GerrymanderEasy {
+    public:
+    double getmax(vector<int> A, vector<int> B, int K) {
+        int n = A.size();
+        int preA[n+1];
+        int preB[n+1];
+        preA[0] = 0;
+        preB[0] = 0;
+        int i,j,k;
+        forl(i,0,n){
+            preA[i+1] = preA[i] + A[i];
+            preB[i+1] = preB[i] + B[i];
+        }
+        double ans = 0.0;
+        forl(i,K,n+1){
+            for(j=i-K;j>=0;j--){
+                double up = preA[i]-preA[j];
+                double down = preB[i]-preB[j];
+                double c = down/up;
+                if(c > ans){
+                    ans = c;
+                }
+            }            
+        }
+        return ans;
+    }
+};
+
+// CUT begin
+ifstream data("GerrymanderEasy.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool double_equal(const double &a, const double &b) { return b==b && a==a && fabs(b - a) <= 1e-9 * max(1.0, fabs(a) ); }
+
+bool do_test(vector<int> A, vector<int> B, int K, double __expected) {
+    time_t startClock = clock();
+    GerrymanderEasy *instance = new GerrymanderEasy();
+    double __result = instance->getmax(A, B, K);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (double_equal(__expected, __result)) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> A;
+        from_stream(A);
+        vector<int> B;
+        from_stream(B);
+        int K;
+        from_stream(K);
+        next_line();
+        double __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(A, B, K, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1462323924;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "GerrymanderEasy (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
