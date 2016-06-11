d3a810033b013c1de23178b6449a154688029a5c
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-06-11 08:41:00 +0600

    Strongly Connected Component (SCC) + binary search

---
+++ SRM 692/HardProof.cpp
@@ -0,0 +1,254 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 55
+#define INF 9999999
+
+vi adj[MAX]; //main graph
+vi rev[MAX]; //reverse graph (with edges reversed)
+bool vis[MAX];
+stack<int> order; //vertex with largest finishing time at the top
+int n;
+int ar[MAX][MAX];
+vector<int> D;
+void dfs(int v){
+    int i;
+    vis[v]=true;
+    int sz = adj[v].size();
+    forl(i,0,sz){
+        if(!vis[adj[v][i]]){
+            dfs(adj[v][i]);
+        }
+    }
+    order.push(v);
+}
+
+void dfs2(int v){
+    int i;
+    vis[v]=true;
+    int sz = rev[v].size();
+    forl(i,0,sz){
+        if( !vis[rev[v][i]]){
+            dfs2(rev[v][i]);
+        }
+    }
+}
+void top(){
+    int i;
+    ms(vis,false);
+    forl(i,0,n){
+        if(!vis[i]){
+            dfs(i);
+        }
+    }   
+}
+
+//check if only a single Strongly Connected component
+bool isSingleSCC(int l, int r){
+    int i,j;
+    forl(j,0,n){
+        adj[j].clear();
+        rev[j].clear();
+    }
+    forl(i,0,n){
+        forl(j,0,n){
+            if(l <= ar[i][j] && ar[i][j] <= r){
+                adj[i].pb(j);
+                rev[j].pb(i);
+            }
+        }
+    }
+    
+    ms(vis,false);
+    while(!order.empty())
+        order.pop();
+    top();
+    int component_count = 0;
+    ms(vis,false);
+    while(!order.empty()){
+        if(component_count>1)
+            return false;
+        int vertex = order.top();
+        order.pop();
+        if(!vis[vertex]){
+            component_count++;
+            dfs2(vertex);
+        }
+    }
+    if(component_count==1)
+        return true;
+    return false;
+}
+bool taken[150001];
+class HardProof {
+    public:
+    int minimumCost(vector<int> _D) {
+        int ans = 99999999;
+        D = _D;
+        n = sqrt(_D.size()*1.0);
+        int i,j,k,low,high,mid;
+        int sz = _D.size();
+        forl(i,0,n){
+            forl(j,0,n){
+                ar[i][j] = _D[i*n+j];
+            }
+        }
+        ms(taken,false);
+        forl(i,0,sz){
+            if(taken[_D[i]])
+                continue;
+            taken[_D[i]]=true;
+            low = _D[i];
+            high = 999999999;
+            int res=-1;
+            while(low<=high){
+                mid = (low+high)>>1;
+                
+                if(isSingleSCC(_D[i],mid)){
+                    res = mid;
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }
+            if(res != -1){
+                ans = min(ans,res-_D[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+// CUT begin
+ifstream data("HardProof.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> D, int __expected) {
+    time_t startClock = clock();
+    HardProof *instance = new HardProof();
+    int __result = instance->minimumCost(D);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> D;
+        from_stream(D);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(D, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1465607801;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "HardProof (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
