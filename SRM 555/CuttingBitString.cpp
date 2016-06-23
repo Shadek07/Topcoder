a6220e448325851cce61ee51c8e9f5acafd174eb
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-06-23 15:27:03 +0600

    DP, Simple Search, Iteration

---
+++ SRM 555/CuttingBitString.cpp
@@ -0,0 +1,180 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 55
+#define INF 9999999
+
+int dp[MAX];
+int n;
+string s;
+
+bool isFivePower(ull n){ //check if n is a power of 5 i.e 1, 5 , 25, 125 ... etc
+    int i;
+    ull m = 1;
+    while(true){
+        if(m == n)
+            return true;
+        if(m>n)
+            return false;
+        m *= 5;
+    }
+    return false;
+}
+int doit(int ind){
+    if(ind == n){
+        return 0;
+    }
+    int &res = dp[ind];
+    if(s[ind] == '0'){ //substring should not have any leading zero
+        res = -1;
+        return res;
+    }
+    ull v = 0;
+    int i;
+    for(i = ind; i < n;i++){
+        v = 2*v + (s[i]-'0');
+        if(isFivePower(v)){ //substring from ind to i creates a power of 5
+            int tmp = doit(i+1);
+            if(tmp != -1){
+                if(res == -1)
+                    res = 1 + tmp;
+                else
+                    res = min(res, 1 + tmp);
+            }
+        }
+    }
+    return res;
+}
+
+class CuttingBitString {
+    public:
+    int getmin(string S) {
+        n = S.size();
+        ms(dp,-1);
+        s = S;
+        return doit(0);
+    }
+};
+
+// CUT begin
+ifstream data("CuttingBitString.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string S, int __expected) {
+    time_t startClock = clock();
+    CuttingBitString *instance = new CuttingBitString();
+    int __result = instance->getmin(S);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string S;
+        from_stream(S);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(S, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1466670054;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 255 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "CuttingBitString (255 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
