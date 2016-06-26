39feda13ddc40f2c5746dbf88d52e6476b3c2e9b
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-06-27 02:36:30 +0600

    greedy

---
+++ SRM 572/NewArenaPassword.cpp
@@ -0,0 +1,165 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+#define INF 9999999
+
+
+class NewArenaPassword {
+    public:
+    int minChange(string oldPassword, int K) {
+        int n = oldPassword.size();
+        int i,j,k;
+        int ans=0;
+        if(2*K <= n){ //no overlap
+            i = 0;
+            j = n-K;
+            for(;j<n;i++,j++){
+                ans = ans + (oldPassword[i] != oldPassword[j]);
+            }
+            return ans;
+        }
+        
+        int step = n - K;
+        for(i=0;i<step;i++){
+            int arr[26];
+            ms(arr,0);
+            int mx=0;
+            //(j, j+step, j+2*step, j+3*step ...), letters at these indexes should be all equal
+            //so a letter that appears most number of times in these indexes shouldn't be changed
+            //change all other letters to the most frequent letter
+            for(j=i;j<n;j+=step){
+                arr[oldPassword[j]-'a']++;
+                mx = max(mx,arr[oldPassword[j]-'a']);
+            }
+            int total = 1+(n-1-i)/step; //total indexes
+            ans += (total-mx); //letters that need to change
+        }
+        return ans;
+    }
+};
+
+// CUT begin
+ifstream data("NewArenaPassword.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string oldPassword, int K, int __expected) {
+    time_t startClock = clock();
+    NewArenaPassword *instance = new NewArenaPassword();
+    int __result = instance->minChange(oldPassword, K);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string oldPassword;
+        from_stream(oldPassword);
+        int K;
+        from_stream(K);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(oldPassword, K, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1466969934;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "NewArenaPassword (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
