299332e7dd0b555968bee488e8b883211b8360ad
shadek_cse <shadekcse07@gmail.com>
2017-08-27 16:02:32 -0500

    div 2 easy (math)

---
+++ Single Round Match 716/Permutiple.cpp
@@ -0,0 +1,184 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+#include<cctype>
+#include<algorithm>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<list>
+#include <bitset>
+#include<iomanip>
+#include <fstream>
+#include<ctime>
+
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define MX 1000000
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+
+
+class Permutiple {
+    public:
+    string isPossible(int x) {
+        string p = "Possible";
+        string imp = "Impossible";
+        int i,j;
+        //count_x[i] is the counter number for digit i in number x
+        int count_x[10];
+        int a = x;
+        memset((count_x),(0),sizeof(count_x)); //initialize x's counter array to zero's
+        while(a){
+            count_x[a%10]++;
+            a /= 10;
+        }
+        forl(i,2,10){
+            int y = x*i;
+            //count_y[i] is the counter number for digit i in number y
+            int count_y[10];
+            memset((count_y),(0),sizeof(count_y)); //initialize array to zero's
+            while(y){
+                count_y[y%10]++;
+                y /= 10;
+            }
+            bool ok=true;
+            forl(j,0,10){
+                if(count_x[j] != count_y[j]){ //check if digit j has equal counter in both x and y
+                    ok=false;
+                    break;
+                }
+            }
+            if(ok) //return 'Possible' if both counter arrays are same
+                return p;
+        }
+        return imp; //Otherwise 'Impossible'
+    }
+};
+
+// CUT begin
+ifstream data("Permutiple.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int x, string __expected) {
+    time_t startClock = clock();
+    Permutiple *instance = new Permutiple();
+    string __result = instance->isPossible(x);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int x;
+        from_stream(x);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(x, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1498525209;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "Permutiple (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
