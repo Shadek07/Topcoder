872fc759062f6fbfc2550378ffca8b6bc2e71d6e
Shadek <shadekcse07@gmail.com>
2021-02-09 00:22:48 -0500

    div2 medium math(power)

--- SRM 799/PlanningTrips.cpp
+++ SRM 799/PlanningTrips.cpp
@@ -1,95 +1,40 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
 #include <algorithm>
 #include <set>
 #include <vector>
 #include <sstream>
 #include <typeinfo>
 #include <fstream>
+#include<map>
 using namespace std;
 
-int grid[51][51];
-int n,m;
-int dx[] = {0, -1, -1, -1, 0, 1, 1, 1};
-int dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
-int cnt = 0;
-class IslandInALake {
+class PlanningTrips {
     public:
-    bool adjacent(int a, int b){
-        int i,x,y;
-        for(i=0;i<8;i+=1){
-            x = a+dx[i];
-            y = b+dy[i];
-            if(x >=0 and x<n and y >= 0 and y < m){
-                if(grid[x][y] == 1){
-                    return true;
-                }
+    int find(int a, vector<int> num) {
+        map<int, int> mp;
+        map<int,int>::iterator it;
+        for (auto n : num){
+            mp[n]++;
+            while (mp[n] == a){
+               mp.erase (n);   
+               n++;
+               mp[n]++;
             }
         }
-        return false;
-    }
-    void watercnt(int a, int b){
-        cnt += 1;
-        grid[a][b] = 2;
-        int i,x,y;
-        for(i=0;i<8;i++){
-            x = a+dx[i];
-            y = b+dy[i];
-            if(x>=0 and x<n and y>=0 and y<m and grid[x][y] == 0 and !adjacent(x,y)){
-                watercnt(x,y);
-            }
+        if (mp.size() == 1 and mp.begin()->second == 1){
+            return mp.begin()->first;
         }
+        
+        return mp.rbegin()->first+1;
     }
-    void makeInvalid(int a, int b){
-        int i,j,x,y;
-        grid[a][b] = 2;
-        for(i=0;i<8;i+=2){
-            x = a + dx[i];
-            y = b+ dy[i];
-            if (x >= 0 and x < n and y >=0 and y<m){
-                if(grid[x][y] == 0){
-                    makeInvalid(x,y);
-                }
-            }
-        }
-    }
-    int build(vector<string> country) {
-	n = country.size();
-	m = country[0].size();
-	if(n == 1 or m == 1){
-		return 0;
-    }
-    int i,j,k;
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            if (country[i][j] == '.')
-                grid[i][j] = 0;
-            else
-                grid[i][j]=1;
-        }
-    }
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            if(i==0 or i==n-1 or j==0 or j==m-1){
-                makeInvalid(i,j);
-            }
-        }
-    }
-    int ans = 0;
-
-    for(i = 1;i<n-1;i++){
-        for(j=1;j<m-1;j++){
-            if(grid[i][j] == 0 and !adjacent(i,j)){
-                cnt = 0;
-                watercnt(i,j);
-                ans = max(ans, cnt);
-            }
-        }
-    }
-    return ans;
-  }
 };
 
 // CUT begin
-ifstream data("IslandInALake.sample");
+ifstream data("PlanningTrips.sample");
 
 string next_line() {
     string s;
@@ -128,10 +73,10 @@ string to_string(string t) {
     return "\"" + t + "\"";
 }
 
-bool do_test(vector<string> country, int __expected) {
+bool do_test(int a, vector<int> num, int __expected) {
     time_t startClock = clock();
-    IslandInALake *instance = new IslandInALake();
-    int __result = instance->build(country);
+    PlanningTrips *instance = new PlanningTrips();
+    int __result = instance->find(a, num);
     double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
     delete instance;
 
@@ -152,8 +97,10 @@ int run_test(bool mainProcess, const set<int> &case_set, const string command) {
     while (true) {
         if (next_line().find("--") != 0)
             break;
-        vector<string> country;
-        from_stream(country);
+        int a;
+        from_stream(a);
+        vector<int> num;
+        from_stream(num);
         next_line();
         int __answer;
         from_stream(__answer);
@@ -163,16 +110,16 @@ int run_test(bool mainProcess, const set<int> &case_set, const string command) {
             continue;
 
         cout << "  Testcase #" << cases - 1 << " ... ";
-        if ( do_test(country, __answer)) {
+        if ( do_test(a, num, __answer)) {
             passed++;
         }
     }
     if (mainProcess) {
         cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
-        int T = time(NULL) - 1607162353;
+        int T = time(NULL) - 1612838195;
         double PT = T / 60.0, TT = 75.0;
         cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
-        cout << "Score  : " << 900 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
     }
     return 0;
 }
@@ -190,13 +137,8 @@ int main(int argc, char *argv[]) {
         }
     }
     if (mainProcess) {
-        cout << "IslandInALake (900 Points)" << endl << endl;
+        cout << "PlanningTrips (500 Points)" << endl << endl;
     }
     return run_test(mainProcess, cases, argv[0]);
 }
 // CUT end
-#include <cstdio>
-#include <cmath>
-#include <cstring>
-#include <ctime>
-#include <iostream>
